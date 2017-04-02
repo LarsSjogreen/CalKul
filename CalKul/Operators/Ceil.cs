325acd534536b29dc815844b8b648c257db27c9d
Lars Sjögreen <Lars Sjgreen>
2017-04-02 10:40:22 +0200

    Adding Ceil and Floor

---
+++ CalKul/Operators/Ceil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul
+{
+    public class Ceil : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 1; }
+        }
+
+        public string OperandName
+        {
+            get { return "ceil"; }
+        }
+
+        public double Do(Stack<double> args)
+        {
+            OperatorUtils.CheckArgs(args, this);
+            return Math.Ceiling(args.Pop());
+        }
+    }
+}
