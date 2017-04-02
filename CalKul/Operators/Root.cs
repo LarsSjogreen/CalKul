91bf55f56f66428e643673bffcdf681fabb0e5fa
eliassjogreen <eliassjogreen1@gmail.com>
2017-04-02 17:12:01 +0200

    Added root operator.

---
+++ CalKul/Operators/Root.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul
+{
+    class Root : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public string OperandName
+        {
+            get
+            {
+                return "root";
+            }
+        }
+
+        public double Do(Stack<double> args)
+        {
+            OperatorUtils.CheckArgs(args, this);
+            return Math.Sqrt(args.Pop());
+        }
+    }
+}
