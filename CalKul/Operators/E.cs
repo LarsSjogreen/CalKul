bf6b8875094c27e7ec5d22b1b3252f63bc42eeb9
Lars Sjögreen <Lars Sjgreen>
2017-04-02 10:04:34 +0200

    Added e.

---
+++ CalKul/Operators/E.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul
+{
+    public class E : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public string OperandName
+        {
+            get
+            {
+                return "e";
+            }
+        }
+
+        public double Do(Stack<double> args)
+        {
+            OperatorUtils.CheckArgs(args, this);
+            return Math.E;
+        }
+    }
+}
