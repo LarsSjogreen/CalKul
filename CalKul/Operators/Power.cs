0fc32530b9c6660e67ab4453bb403240ae6ece3b
eliassjogreen <eliassjogreen1@gmail.com>
2017-04-02 21:30:55 +0200

    Adding power and nth root operators.

---
+++ CalKul/Operators/Power.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul
+{
+    public class Power : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 2; }
+        }
+
+        public string OperandName
+        {
+            get { return "pow"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            OperatorUtils.CheckNumberOfArguments(args, this.NumberOfArguments);
+            var y = (double)args.Pop();
+            var x = (double)args.Pop();
+            return Math.Pow(x, y);
+        }
+    }
+}
---
+++ CalKul/Operators/Xroot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul
+{
+    public class Xroot : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 2; }
+        }
+
+        public string OperandName
+        {
+            get { return "xroot"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            OperatorUtils.CheckNumberOfArguments(args, this.NumberOfArguments);
+            var x = (double)args.Pop();
+            var y = (double)args.Pop();
+            return Math.Pow(y, 1.0 / x);
+        }
+    }
+}
