453d74310380c2d70dd3e0f29f163b08dc555945
Lars Sjögreen <Lars Sjgreen>
2017-04-07 14:25:39 +0200

    Added < > and ==

---
+++ CalKul/Operators/MoreThan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul
+{
+    class MoreThan : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 2; }
+        }
+
+        public string OperandName
+        {
+            get { return ">"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            OperatorUtils.CheckArgs(args, this);
+            var second = args.Pop();
+            return ((double)(args.Pop()) > (double)second);
+        }
+    }
+}
