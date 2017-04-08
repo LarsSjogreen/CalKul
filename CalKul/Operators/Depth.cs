67290634ca74826151c52777ddbd21549dd72ecf
Lars Sjögreen <Lars Sjgreen>
2017-04-08 10:35:41 +0200

    Added depth and ! (factorial).

---
+++ CalKul/Operators/Depth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul.Operators
+{
+    class Depth : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 0; }
+        }
+
+        public string OperandName
+        {
+            get { return "depth"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            return args.Count;
+        }
+    }
+}
---
+++ CalKul/Operators/Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul.Operators
+{
+    class Factorial : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 1; }
+        }
+
+        public string OperandName
+        {
+            get { return "!"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            OperatorUtils.CheckArgs(args, this);
+            var value = (double)args.Pop();
+            double result = 1.0;
+            for (var i = value; i>0;i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
