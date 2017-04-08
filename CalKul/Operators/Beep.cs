7467a6f5b04295329fc83deded1e8bc6f8b64326
eliassjogreen <eliassjogreen1@gmail.com>
2017-04-08 21:38:50 +0200

    Added beep and wait. Wait still needs polishing (wait until keypress and wait seconds. Also wait should be able to break on enter).

---
+++ CalKul/Operators/Beep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul.Operators
+{
+    class Beep : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 2; }
+        }
+
+        public string OperandName
+        {
+            get { return "beep"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            double duration = (double)args.Pop()*1000;
+            double frequency = (double)args.Pop();
+            
+            Console.Beep((int)frequency, (int)duration);
+            return null;
+        }
+    }
+}
---
+++ CalKul/Operators/Wait.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CalKul.Operators
+{
+    public class Wait : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 1; }
+        }
+
+        public string OperandName
+        {
+            get { return "wait"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            double duration = (double)args.Pop();
+            Thread.Sleep((int)(duration * 1000));
+            return null;
+        }
+    }
+}
