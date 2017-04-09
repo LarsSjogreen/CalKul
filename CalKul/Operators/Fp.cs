f0cb754370d34e726eeea6e573156487d2699dde
Lars Sjögreen <Lars Sjgreen>
2017-04-09 21:11:49 +0200

    Added fp and ip (fractal part, integer part). A bit trickier than I thought.

---
+++ CalKul/Operators/Fp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul.Operators
+{
+    class Fp : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get { return 1; }
+        }
+
+        public string OperandName
+        {
+            get { return "fp"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            OperatorUtils.CheckArgs(args, this);
+            var number = (double)(args.Pop());
+            if (number > 0)
+            {
+                return number - Math.Floor(number);
+            }
+            else
+            {
+                return - Math.Ceiling(number) + number;
+            }
+        }
+    }
+}
