cf021352d240dc58f574375c4abc75803428c6d0
Lars Sjögreen <Lars Sjgreen>
2017-04-09 20:55:46 +0200

    Now the (visual) depth of the stack can be set using a config ('stackrows')

--- CalKul/ConsoleUserInterface.cs
+++ CalKul/ConsoleUserInterface.cs
@@ -9,12 +9,18 @@ namespace CalKul
     class ConsoleUserInterface : IUserInterface, IConfigurable
     {
         IConfigurator _configurator;
+        int showDatatypes;
+        int showLineNumbers;
+        int stackRows;
 
         public ConsoleUserInterface(IConfigurator configurator)
         {
             _configurator = configurator;
             _configurator.Subscribe(this.UpdateConfigs, "windowheight");
             _configurator.Subscribe(this.UpdateConfigs, "windowwidth");
+            _configurator.Subscribe(this.UpdateConfigs, "showdatatypes");
+            _configurator.Subscribe(this.UpdateConfigs, "showlinenumbers");
+            _configurator.Subscribe(this.UpdateConfigs, "stackrows");
             UpdateConfigs();
         }
 
@@ -29,24 +35,43 @@ namespace CalKul
             Console.WindowWidth = wid;
             int heig = _configurator.ReadConfig("windowheight", 20);
             Console.WindowHeight = heig;
+            showDatatypes = _configurator.ReadConfig("showdatatypes", 1);
+            showLineNumbers = _configurator.ReadConfig("showlinenumbers", 1);
+            stackRows = _configurator.ReadConfig("stackrows", 10);
         }
 
         public void WriteStack(Stack<object> stack)
         {
-            int numRows = 8;
-            int counter = numRows;
+            int counter = stackRows;
 
             Clear();
             var showLevels = stack.Count;
-            if (showLevels > numRows)
+            if (showLevels > stackRows)
             {
-                showLevels = numRows;
+                showLevels = stackRows;
             }
 
             var stackView = stack.Take(showLevels).Reverse();
-            for (var i = 0; i < (numRows - showLevels); i++)
+            for (var i = 0; i < (stackRows - showLevels); i++)
             {
-                Console.WriteLine(counter--.ToString() + ": :");
+                if (showLineNumbers == 1 && showDatatypes == 1)
+                {
+                    if (stackRows > 9 && counter < 10) { Console.Write(" "); }
+                    Console.WriteLine(counter--.ToString() + ": :");
+                }
+                else if (showLineNumbers == 0 && showDatatypes == 1)
+                {
+                    Console.WriteLine(" :");
+                }
+                else if (showLineNumbers == 1 && showDatatypes == 0)
+                {
+                    if (stackRows > 9 && counter < 10) { Console.Write(" "); }
+                    Console.WriteLine(counter--.ToString() + ":");
+                }
+                else
+                {
+                    Console.WriteLine(":");
+                }
             }
             foreach (var stackItem in stackView)
             {
@@ -54,7 +79,24 @@ namespace CalKul
                 typ = typ.Remove(0, 7);
                 typ = typ.ToLower();
                 typ = typ.Remove(1);
-                Console.WriteLine(counter--.ToString() + ":" + typ + ": " + stackItem.ToString());
+                if (showLineNumbers == 1 && showDatatypes == 1)
+                {
+                    if (stackRows > 9 && counter < 10) { Console.Write(" "); }
+                    Console.WriteLine(counter--.ToString() + ":" + typ + ": " + stackItem.ToString());
+                }
+                else if (showLineNumbers == 0 && showDatatypes == 1)
+                {
+                    Console.WriteLine(typ + ": " + stackItem.ToString());
+                }
+                else if (showLineNumbers == 1 && showDatatypes == 0)
+                {
+                    if (stackRows > 9 && counter < 10) { Console.Write(" "); }
+                    Console.WriteLine(counter--.ToString() + ": " + stackItem.ToString());
+                }
+                else if (showLineNumbers == 0 && showDatatypes == 0)
+                {
+                    Console.WriteLine(": " + stackItem.ToString());
+                }
             }
             Console.WriteLine("-----------------------------------");
         }
