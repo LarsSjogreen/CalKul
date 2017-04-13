27339e973491c86309321f36d1af3b275a70abe7
eliassjogreen <eliassjogreen1@gmail.com>
2017-04-13 19:04:20 +0200

    Added tau (Pi*2)

---
+++ CalKul/Operators/Tau.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalKul.Operators
+{
+    public class Tau : IOperator
+    {
+        public int NumberOfArguments
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public string OperandName
+        {
+            get { return "tau"; }
+        }
+
+        public object Do(Stack<object> args)
+        {
+            OperatorUtils.CheckArgs(args, this);
+            return Math.PI*2;
+        }
+    }
+}
