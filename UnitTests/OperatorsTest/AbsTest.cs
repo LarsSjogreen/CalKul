8f8caa734067a6efe54cf374c0c7f99705405253
Lars Sjögreen <barse@helgo.net>
2017-05-13 18:38:47 +0200

    Adding a test, just to get going.

---
+++ UnitTests/OperatorsTest/AbsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalKul;
+using System.Collections;
+using System.Collections.Generic;
+using CalKul.Operators;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AbsTest
+    {
+        Abs abs;
+        [TestInitialize]
+        public void Initialize()
+        {
+            abs = new Abs();
+        }
+
+        [TestMethod]
+        public void DoesntChangePositiveNums()
+        {
+            var stack = new Stack<object>();
+            stack.Push(10.0);
+            Assert.AreEqual(10.0, abs.Do(stack));
+        }
+
+        [TestMethod]
+        public void MakesNegativePositive()
+        {
+            var stack = new Stack<object>();
+            stack.Push(-10.0);
+            Assert.AreEqual(10.0, abs.Do(stack));
+        }
+    }
+}
